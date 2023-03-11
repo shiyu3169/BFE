2d89dfc46df47b6e2696ed3453e499e7b3cfeeef
Shiyu Wang <shiyu3169@gmail.com>
2023-03-11 15:20:52 -0800

    9

---
+++ BFE/JavaScript Quiz/8. Implicit Coercion I.js
@@ -0,0 +1,12 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log(Boolean('false')) // true
+console.log(Boolean(false)) // false
+console.log('3' + 1) // 31
+console.log('3' - 1) // 2
+console.log('3' - ' 02 ') // 1
+console.log('3' * ' 02 ') // 6
+console.log(Number('1')) // 1
+console.log(Number('number')) // NaN
+console.log(Number(null)) // 0
+console.log(Number(false)) // 0
