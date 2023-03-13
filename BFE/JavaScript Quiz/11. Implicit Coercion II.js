f34640b3d8f2e9c7d0bf34ab3ee13c66ca34550f
Shiyu Wang <shiyu3169@gmail.com>
2023-03-12 19:52:51 -0700

    11

---
+++ BFE/JavaScript Quiz/11. Implicit Coercion II.js
@@ -0,0 +1,14 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log([] + []) // ""
+console.log([] + 1) // "1"
+console.log([[]] + 1) // "1"
+console.log([[1]] + 1) // "11"
+console.log([[[[2]]]] + 1) // "21"
+console.log([] - 1) // -1
+console.log([[]] - 1) // -1
+console.log([[1]] - 1) // 0
+console.log([[[[2]]]] - 1) // 1
+console.log([] + {}) // "[object Object]"
+console.log({} + {}) // "[object Object][object Object]"
+console.log({} - {}) // NaN
