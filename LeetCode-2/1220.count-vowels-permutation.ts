9037f4209e77805d325564c972806ef4e76db51a
Shiyu Wang <shiyu3169@gmail.com>
2023-04-21 18:27:23 -0700

    update 1220

--- LeetCode-2/1220.count-vowels-permutation.ts
+++ LeetCode-2/1220.count-vowels-permutation.ts
@@ -7,20 +7,23 @@
 // @lc code=start
 function countVowelPermutation(n: number): number {
   const MOD = BigInt(10 ** 9 + 7)
-  const dp = [[], [1, 1, 1, 1, 1]]
+  const dp = [[1, 1, 1, 1, 1]]
   const [a, e, i, o, u] = [0, 1, 2, 3, 4]
   for (let j = 2; j <= n; j++) {
     dp.push([0, 0, 0, 0, 0])
-    dp[j][a] = Number(BigInt(dp[j - 1][e] + dp[j - 1][i] + dp[j - 1][u]) % MOD)
-    dp[j][e] = Number(BigInt(dp[j - 1][a] + dp[j - 1][i]) % MOD)
-    dp[j][i] = Number(BigInt(dp[j - 1][e] + dp[j - 1][o]) % MOD)
-    dp[j][o] = dp[j - 1][i]
-    dp[j][u] = Number(BigInt(dp[j - 1][i] + dp[j - 1][o]) % MOD)
+    dp[1][a] = Number(BigInt(dp[0][e] + dp[0][i] + dp[0][u]) % MOD)
+    dp[1][e] = Number(BigInt(dp[0][a] + dp[0][i]) % MOD)
+    dp[1][i] = Number(BigInt(dp[0][e] + dp[0][o]) % MOD)
+    dp[1][o] = dp[0][i]
+    dp[1][u] = Number(BigInt(dp[0][i] + dp[0][o]) % MOD)
+    dp.shift()
   }
-  return Number(BigInt(dp[n].reduce((acc, cur) => acc + cur, 0)) % MOD)
+  return Number(BigInt(dp.pop()!.reduce((acc, cur) => acc + cur, 0)) % MOD)
 }
 // @lc code=end
-/* Accepted
-43/43 cases passed (169 ms)
+/* 
+Accepted
+43/43 cases passed (144 ms)
 Your runtime beats 76.92 % of typescript submissions
-Your memory usage beats 53.85 % of typescript submissions (50.6 MB) */
+Your memory usage beats 69.23 % of typescript submissions (49.1 MB)
+*/
